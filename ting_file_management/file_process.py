5bdff56c7c7a53c5a52931ac72b84eb083046816
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-02-03 17:21:40 -0300

    feat: F process no M file_process - Requisito 3; - Recebe um arquivo e uma intancia de Queue, utiliza a função txt importer e adiciona o resultado a instancia.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(0, instance.__len__()):
+        data = instance.search(i)
+        if data["nome_do_arquivo"] == path_file:
+            return (- 1)
+
+    lines = txt_importer(path_file)
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+    
+    instance.enqueue(file_dict)
+    print(file_dict, file=sys.stdout)
 
 
 def remove(instance):
