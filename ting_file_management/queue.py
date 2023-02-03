b485a3efec691a7b7a365843a2c94318ff902886
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-02-03 15:17:11 -0300

    feat: Classe Queue e seus métodos - Requisito 1. - Quando instanciada, monta uma lista FIFO, first in first out.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.__queue_data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.__queue_data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.__queue_data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__len__() == 0:
+            return None
+        return self.__queue_data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > (self.__len__() - 1):
+            raise IndexError("Invalid index")
+        return self.__queue_data[index]
