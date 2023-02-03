dc70bba73b79a798d32cef9a5bf5a7e4f8c712de
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-02-03 15:55:03 -0300

    feat: função  txt_importer - Requisito 2; - função no módulo file_management.py; - Ao executar p modulo  pelo terminal junto com o path de um arquivo .txt, retorna uma lista com as informações de cada linha

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    try:
+        with open(path_file, "r") as file:
+            content = file.read()
+            lines = content.split(sep="\n")
+            return lines
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
+
+
+if __name__ == "__main__":
+    __unused, path = sys.argv
+    txt_importer(path)
