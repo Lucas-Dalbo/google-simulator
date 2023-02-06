4b2bce60ee2f9e663b0a43c9b71ec677674e3d54
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-02-06 09:48:51 -0300

    feat: função exits_word - Módulo word_search. - Requisito 7. - Parâmetros: uma string 'word' e uma instância da classe Queue 'instance'. - Retorna uma lista de dicionários que contem a palavra 'word', o nome do arquivo e as linhas em que a a palavra 'word' foi encontrada. - Função auxiliar: find_lines.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
+def find_lines(file, word):
+    lines = []
+    for line_id in (0, file["qtd_linhas"] - 1):
+        line = file["linhas_do_arquivo"][line_id].lower()
+        if line.find(word.lower()) >= 0:
+            line_obj = {"linha": line_id + 1}
+            lines.append(line_obj)
+    return lines
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue_len = instance.__len__()
+    result = []
+    for i in range(0, queue_len):
+        file = instance.search(i)
+        lines = find_lines(file, word)
+        if len(lines) > 0:
+            file_obj = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": lines,
+            }
+            result.append(file_obj)
+    return result
 
 
 def search_by_word(word, instance):
